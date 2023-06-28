bea469a1d6ddd891c2cb3da1e385992550a3489d
pwnaxe <biuro.norsk@gmail.com>
2023-06-28 13:41:12 +0200

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { Box, TextField, Button, Container, Typography, Grid } from '@mui/material';
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <Container component="section" maxWidth="lg" sx={{ py: 8 }}>
+      <Typography variant="h2" align="center">
+        Skontaktuj się z nami
+      </Typography>
+
+      <Box sx={{ mt: 6, mb: 4 }}>
+        <Typography variant="body1" align="center">
+          Chcesz dowiedzieć się więcej o naszych usługach, współpracy lub potrzebujesz wsparcia technicznego? Skontaktuj się z nami!
+        </Typography>
+      </Box>
+
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={12} sm={8} md={6}>
+          <Box component="form" noValidate autoComplete="off">
+            <TextField fullWidth label="Imię i Nazwisko" margin="normal" variant="outlined" />
+            <TextField fullWidth label="Adres e-mail" margin="normal" variant="outlined" />
+            <TextField fullWidth label="Temat" margin="normal" variant="outlined" />
+            <TextField
+              fullWidth
+              label="Wiadomość"
+              margin="normal"
+              variant="outlined"
+              multiline
+              rows={4}
+            />
+            <Button variant="contained" color="primary" type="submit" sx={{ mt: 3 }}>
+              Wyślij wiadomość
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
-}
+};
 
 export default Contact;
--- src/components/Fotter.js
+++ src/components/Fotter.js
@@ -1,11 +1,65 @@
 import React from 'react';
+import { Box, Container, Typography, Grid, Link } from '@mui/material';
 
-const Fotter = () => {
+const Footer = () => {
   return (
-    <div>
-      <h1>Fotter</h1>
-    </div>
-  );
-}
+    <Box component="footer" sx={{ py: 6, backgroundColor: 'primary.main', color: 'white' }}>
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={6} md={3}>
+            <Typography variant="h6">O nas</Typography>
+            <Typography>
+              SC Data Hub to innowacyjna platforma oferująca otwarte dane miejskie dla przedsiębiorców i deweloperów.
+            </Typography>
+          </Grid>
 
-export default Fotter;
+          <Grid item xs={12} sm={6} md={3}>
+            <Typography variant="h6">Linki</Typography>
+            <ul style={{ listStyle: 'none', padding: 0 }}>
+              <li>
+                <Link href="#" color="inherit" underline="none">Strona główna</Link>
+              </li>
+              <li>
+                <Link href="#o-projekcie" color="inherit" underline="none">O Projekcie</Link>
+              </li>
+              <li>
+                <Link href="#api" color="inherit" underline="none">API</Link>
+              </li>
+              <li>
+                <Link href="#kontakt" color="inherit" underline="none">Kontakt</Link>
+              </li>
+            </ul>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <Typography variant="h6">Kontakt</Typography>
+            <Typography>Adres: ul. Marszałkowska 142, 00-000 Warszawa</Typography>
+            <Typography>Email: kontakt@scdatahub.com</Typography>
+            <Typography>Telefon: +48 123 456 789</Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <Typography variant="h6">Social Media</Typography>
+            <Link href="#" color="inherit" underline="none" style={{ marginRight: 8 }}>
+              Facebook
+            </Link>
+            <Link href="#" color="inherit" underline="none" style={{ marginRight: 8 }}>
+              Twitter
+            </Link>
+            <Link href="#" color="inherit" underline="none">
+              LinkedIn
+            </Link>
+          </Grid>
+        </Grid>
+
+        <Box mt={5}>
+          <Typography variant="body2" color="white" align="center">
+            © {new Date().getFullYear()} SC Data Hub. Wszystkie prawa zastrzeżone.
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
