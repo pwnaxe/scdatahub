c23bfcede47d8c7173021a7200aa10b1b9a68e81
pwnaxe <biuro.norsk@gmail.com>
2023-06-29 11:50:21 +0200

    up

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,16 +1,64 @@
-import React from 'react';
-import { Box, Typography, Paper, Grid, Container, Button } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Box,
+  Typography,
+  Paper,
+  Grid,
+  Container,
+  Button,
+  Drawer,
+  Card,
+  CardContent
+} from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const GlassPaper = styled(Paper)(({ theme }) => ({
-  backgroundColor: 'rgba(255, 255, 255, 0.1)',
-  backdropFilter: 'blur(5px)',
+  backgroundColor: 'rgba(53, 86, 155, 0.1)',
+  backdropFilter: 'blur(10px) saturate(125%)',
+  border: '1px solid rgba(255, 255, 255, 0.3)',
+  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
   padding: theme.spacing(2),
   borderRadius: '10px',
   textAlign: 'center',
+  transition: '0.5s',
+
+  '&:hover': {
+    boxShadow: '0 8px 12px rgba(0, 0, 0, 0.1)',
+    transform: 'scale(1.01)',
+  },
 }));
 
 const Oferta = () => {
+  const [drawerOpen, setDrawerOpen] = useState([false, false, false]);
+  const [rotated, setRotated] = useState([false, false, false]);
+
+  const handleDrawerToggle = (index) => {
+    const newDrawerOpen = [...drawerOpen];
+    const newRotated = [...rotated];
+    newDrawerOpen[index] = !newDrawerOpen[index];
+    newRotated[index] = !newRotated[index];
+    setDrawerOpen(newDrawerOpen);
+    setRotated(newRotated);
+  };
+
+  const cards = [
+    {
+      title: 'Dostęp do Otwartych Danych',
+      content: 'Uzyskaj dostęp do szerokiej gamy otwartych danych miejskich, które mogą być wykorzystane w różnych aplikacjach i usługach.',
+      buttonText: 'Dowiedz się więcej'
+    },
+    {
+      title: 'Zaawansowane API',
+      content: 'Nasze API pozwala na szybką i łatwą integrację z naszymi danymi, umożliwiając rozwój innowacyjnych rozwiązań.',
+      buttonText: 'Przetestuj API'
+    },
+    {
+      title: 'Wsparcie i Współpraca',
+      content: 'Nasz zespół ekspertów jest dostępny, aby pomóc w osiągnięciu Twoich celów biznesowych i technicznych.',
+      buttonText: 'Skontaktuj się z nami'
+    }
+  ];
+
   return (
     <Container component="section" maxWidth="lg" sx={{ py: 8 }}>
       <Typography variant="h2" align="center">
@@ -25,46 +73,49 @@ const Oferta = () => {
       </Box>
 
       <Grid container spacing={4} justifyContent="center">
-        <Grid item xs={12} sm={6} md={4}>
-          <GlassPaper elevation={3}>
-            <Typography variant="h5" gutterBottom>
-              Dostęp do Otwartych Danych
-            </Typography>
-            <Typography>
-              Uzyskaj dostęp do szerokiej gamy otwartych danych miejskich, które mogą być wykorzystane w różnych aplikacjach i usługach.
-            </Typography>
-            <Button variant="contained" color="primary" sx={{ mt: 2 }}>
-              Dowiedz się więcej
-            </Button>
-          </GlassPaper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <GlassPaper elevation={3}>
-            <Typography variant="h5" gutterBottom>
-              Zaawansowane API
-            </Typography>
-            <Typography>
-              Nasze API pozwala na szybką i łatwą integrację z naszymi danymi, umożliwiając rozwój innowacyjnych rozwiązań.
-            </Typography>
-            <Button variant="contained" color="primary" sx={{ mt: 2 }}>
-              Przetestuj API
-            </Button>
-          </GlassPaper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <GlassPaper elevation={3}>
-            <Typography variant="h5" gutterBottom>
-              Wsparcie i Współpraca
-            </Typography>
-            <Typography>
-              Nasz zespół ekspertów jest dostępny, aby pomóc w osiągnięciu Twoich celów biznesowych i technicznych.
-            </Typography>
-            <Button variant="contained" color="primary" sx={{ mt: 2 }}>
-              Skontaktuj się z nami
-            </Button>
-          </GlassPaper>
-        </Grid>
+        {cards.map((card, index) => (
+          <Grid key={index} item xs={12} sm={6} md={4}>
+            <GlassPaper elevation={3} style={{
+              transform: rotated[index] ? 'rotateZ(90deg)' : 'none',
+            }}>
+              <Typography variant="h5" gutterBottom>
+                {card.title}
+              </Typography>
+              <Typography>
+                {card.content}
+              </Typography>
+              <Button variant="contained" color="primary" sx={{ mt: 2 }} onClick={() => handleDrawerToggle(index)}>
+                {card.buttonText}
+              </Button>
+            </GlassPaper>
+          </Grid>
+        ))}
       </Grid>
+
+      {drawerOpen.map((isOpen, index) => (
+        <Drawer
+          key={index}
+          anchor="bottom"
+          open={isOpen}
+          onClose={() => handleDrawerToggle(index)}
+        >
+          <Box
+            sx={{ height: 400, width: 250, padding: 2 }}
+            role="presentation"
+            onClick={() => handleDrawerToggle(index)}
+            onKeyDown={() => handleDrawerToggle(index)}
+          >
+            <Card>
+              <CardContent>
+                <Typography variant="h5">Tytuł karty</Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Jakiś przykładowy tekst karty.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        </Drawer>
+      ))}
     </Container>
   );
 };
