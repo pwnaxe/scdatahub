58b7ed5bdb7edce2e8118727dfbdb0954d8e6bc6
pwnaxe <biuro.norsk@gmail.com>
2023-07-20 06:22:22 +0200

    up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, TextField, Button, Container, Typography, Grid } from '@mui/material';
+import { Box, Container, Typography, Grid, Link } from '@mui/material';
 
 const Contact = () => {
   return (
@@ -17,28 +17,19 @@ const Contact = () => {
 
       <Grid container spacing={2} justifyContent="center">
         <Grid item xs={12} sm={8} md={6}>
-          <Box component="form" noValidate autoComplete="off" sx={{
+          <Box sx={{
             py: 6,
             backgroundColor: 'rgba(221, 235, 255, 0.3)',
             backdropFilter: 'blur(10px)',
             color: 'white',
             padding: '20px', borderRadius: '10px'
           }}>
-            <TextField fullWidth label="Imię i Nazwisko" margin="normal" variant="outlined" InputLabelProps={{ style: { color: 'white' } }} />
-            <TextField fullWidth label="Adres e-mail" margin="normal" variant="outlined" InputLabelProps={{ style: { color: 'white' } }} />
-            <TextField fullWidth label="Temat" margin="normal" variant="outlined" InputLabelProps={{ style: { color: 'white' } }} />
-            <TextField
-              fullWidth
-              label="Wiadomość"
-              margin="normal"
-              variant="outlined"
-              multiline
-              rows={4}
-              InputLabelProps={{ style: { color: 'white' } }}
-            />
-            <Button variant="contained" type="submit" sx={{ mt: 3, backgroundColor: '#2f3e63', color: 'white', '&:hover': { backgroundColor: '#0069a7' } }}>
-              Wyślij wiadomość
-            </Button>
+            <Typography variant="body1" textAlign={"center"}>
+              Telefon: <Link href="tel:+48 123 456 789" style={{ color: '#ffffff' }}>+48 123 456 789</Link>
+            </Typography>
+            <Typography variant="body1" textAlign={"center"} sx={{ mt: 2 }}>
+              E-mail: <Link href="mailto:kontakt@domena.pl" style={{ color: '#ffffff' }}>kontakt@domena.pl</Link>
+            </Typography>
           </Box>
         </Grid>
       </Grid>
